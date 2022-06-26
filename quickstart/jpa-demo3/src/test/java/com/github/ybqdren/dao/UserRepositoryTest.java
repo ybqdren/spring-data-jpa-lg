a7ec4755882679cacc5b7ba01a2254efcf46b01d
Wen Zhao (Joan) <withzhaowen@126.com>
2022-06-26 13:18:41 +0800

    update

--- quickstart/jpa-demo3/src/test/java/com/github/ybqdren/dao/UserRepositoryTest.java
+++ quickstart/jpa-demo3/src/test/java/com/github/ybqdren/dao/UserRepositoryTest.java
@@ -1,10 +1,19 @@
-package com.github.ybqdren;
+package com.github.ybqdren.dao;
 
 import com.github.ybqdren.dao.UserRepository;
 import com.github.ybqdren.entity.UserInfo;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.init.DataSourceScriptDatabaseInitializer;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -17,6 +26,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
+
 
     @Test
     public void testSaveUser() {
